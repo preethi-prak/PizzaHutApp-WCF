425346fc97142568a7e3e7220e5e427efd8a625a
preethi-prak <preethi93@icloud.com>
2021-05-12 21:55:40 -0700

    #1 Inital Commit

---
+++ PizzaHut.Services/IPizzaHut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHut.Services
+{
+    [ServiceContract]
+    public interface IPizzaHut
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract]
+        List<Customer> GetCustomers();
+        [OperationContract]
+        void SubmitOrder(Order order); 
+    }
+}
