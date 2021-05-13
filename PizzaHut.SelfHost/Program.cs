44a6874c7a0a758bf57e9ac8e8be841c6371c76e
preethi-prak <preethi93@icloud.com>
2021-05-13 04:36:36 -0700

    #3 Self Hosting

---
+++ PizzaHut.SelfHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using PizzaHut.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHut.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(PizzaHutService));
+            host.Open();
+            Console.WriteLine("Hit Any key to exit");
+            Console.ReadKey();
+            host.Close();
+
+        }
+    }
+}
